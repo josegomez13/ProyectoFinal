f8ab94946d1366dc7b484c840887732c74130bff
danielavilladiego <daniela.villadiegop@udea.edu.co>
2021-10-19 20:28:48 -0500

    se realizaron las funciones que van en la clase distancia dichas funciones son las que van incrementando, obteniendo, reiniciando y asignando las vidas al sprite, lo que hacen es que cuando el sprite tome una gota de agua esta clase sea la encargada de incrementar la vitalidad del personaje, en caso contrario de que un dulce lo toque será la encargada de disminuir sus vidas

--- distancia.h
+++ distancia.h
@@ -10,6 +10,7 @@ public:
     void incrementar(); //Funcion que incrementa las vidas
     int obtenervida(); //Funcion que guarda las vidas
     void reiniciarvidas(); //Funcion que llena las vidas
+    void asignarVidas(int puntos);//Funcion que asigna los puntos
 
 private:
     int metros; //Variable donde se guardan  los puntos
